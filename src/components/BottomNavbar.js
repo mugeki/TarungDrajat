64ad02f9d937ddc01017fd80e93c376c8380cf16
Muhamad Elang Ramadhan <elangmugeki@gmail.com>
2022-03-16 12:10:46 +0700

    remove unused import

--- src/components/BottomNavbar.js
+++ src/components/BottomNavbar.js
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react';
+import React from 'react';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 // import HomeScreen from '../screens/HomeScreen';
